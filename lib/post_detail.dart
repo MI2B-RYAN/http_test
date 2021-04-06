26e78b8fe8cde1656199297e02aaa35b1e4b9fd7
MI2B-RYAN <ryanz.c69@gmail.com>
2021-04-06 18:41:00 +0700

    http_test

---
+++ lib/post_detail.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'post_model.dart';
+import 'http_service.dart';
+
+class PostDetail extends StatelessWidget {
+  final Post post;
+  final HttpService httpService = HttpService();
+
+  PostDetail({
+    @required this.post
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(post.title)
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: () async {
+          await httpService.deletePost(post.id);
+          Navigator.of(context).pop();
+        },
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text("Title"),
+                      subtitle: Text(post.title),
+                    ),
+                    ListTile(
+                      title: Text("ID"),
+                      subtitle: Text("${post.id}"),
+                    ),
+                    ListTile(
+                      title: Text("Body"),
+                      subtitle: Text(post.body),
+                    ),
+                    ListTile(
+                      title: Text("User ID"),
+                      subtitle: Text("${post.userId}"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
